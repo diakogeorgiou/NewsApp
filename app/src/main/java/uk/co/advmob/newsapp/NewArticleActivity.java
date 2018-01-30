bac11021d46ea8c109ca639c6ae2e4136e4ef9c2
Kostas <diakogeorgiou@gmail.com>
2018-01-30 12:51:06 +0000

    Initial

--- app/src/main/java/uk/co/advmob/newsapp/NewArticleActivity.java
+++ app/src/main/java/uk/co/advmob/newsapp/NewArticleActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class NewArticleActivity extends AppCompatActivity {
 
@@ -18,5 +20,18 @@ public class NewArticleActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_new_article, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id==R.id.menu_save_article) {
+            finish();
+            Toast.makeText(this, "Article saved", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
